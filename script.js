8467e744b70c14b65bbfe25cf512c96e6ed98d32
Alex <oleksi.ovsiannikov@gmail.com>
2026-02-04 10:39:32 +0100

    feat: complete level 100

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //You can edit ALL of the code here
+
+  rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const card = document.createElement("section");
+    card.classList.add("card");
+
+    const title = document.createElement("h3");
+    title.textContent = episode.name;
+
+    const createImg = document.createElement("img");
+    createImg.src = episode.image.medium;
+    createImg.alt = episode.name;
+
+    const desc = document.createElement("p");
+    desc.innerHTML = episode.summary;
+
+    card.append(title, createImg, desc);
+    root.append(card);
+  });
 }
+
 
 window.onload = setup;
